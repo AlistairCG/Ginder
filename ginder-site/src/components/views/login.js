2f6c10ab6debee9a26f21de6b3f2b20f1d704fd5
Andrew <andrewpkoung@gmail.com>
2019-10-19 00:45:20 -0400

    template code for reference

---
+++ ginder-site/src/components/views/login.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div style={{ height: '100vh' }}>
+                <div style={wallPaper}>
+                    <div style={loginForm}>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label style={{color: "red"}}>Username</Form.Label>
+                                <Form.Control type="email" placeholder="Enter username" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your username with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label style={{color: "red"}}>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Enter Password" />
+                            </Form.Group>
+
+                            <div style={{textAlign: 'center'}}>
+                                <Button variant="primary" type="submit" className="btn btn-danger">
+                                Link on
+                                </Button>
+                            </div>
+                        </Form>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login
+
+const wallPaper = {
+    backgroundImage: 'url(' + require('../../images/androidBackground.png') + ')',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%'
+};
+
+const loginForm = {
+    width: '40%',
+    paddingTop: '130px',
+    paddingLeft: '150px'
+}
