7ec7a2695f2d25ed24ae06554c0725b0c37a9041
Andrew <andrewpkoung@gmail.com>
2019-10-19 00:41:34 -0400

    missing files

--- ginder-site/src/App.js
+++ ginder-site/src/App.js
@@ -7,10 +7,10 @@ import { Route, Link, BrowserRouter as Router} from 'react-router-dom'
 //importing all components
 import Home from './components/views/home';
 import Discover  from './components/views/discover';
-import Groups  from './components/views/groups';
+import Groups  from './components/views/groups.js';
 import Homepage  from './components/views/userHomePage';
 import Login  from './components/views/login';
-import MyGroups  from './components/views/mygroups';
+import MyGroups  from './components/views/myGroups';
 import NewGroup  from './components/views/newgroup';
 import Registration  from './components/views/registration';
 
