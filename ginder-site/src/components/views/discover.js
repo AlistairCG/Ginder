80a051ffd0e075d72f029474dfdb8ccf2dbc113f
Andrew <andrewpkoung@gmail.com>
2019-10-19 00:33:38 -0400

    adding sub files

---
+++ ginder-site/src/components/views/discover.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Discover extends React.Component {
+    render() {
+        return <h1>Discover</h1>
+    }
+}
+
+export default Discover
---
+++ ginder-site/src/components/views/userHomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Homepage extends React.Component {
+    render() {
+        return (
+            <p>Hi</p>
+        );
+    }
+}
+
+export default Homepage
