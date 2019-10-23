a0c9a23989c9d1f51c0ff3f8ddca1d56a57f7c32
Jaideep Sidhu <jaideep@Jaideeps-MacBook-Pro.local>
2019-10-23 15:58:09 -0400

    Added content for user's homepage

--- ginder-site/src/components/views/userHomePage.js
+++ ginder-site/src/components/views/userHomePage.js
@@ -1,16 +1,76 @@
-import React from 'react'
+import React from "react";
+import { Tab, Row, Col, Nav } from "react-bootstrap";
+import Header from "../utils/header";
 
-import Header from '../utils/header'
+class UserHomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header isGuest="false" />
+        <Tab.Container defaultActiveKey="profile">
+          <Row>
+            <Col sm={3}>
+              <Nav fill variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="profile" >Profile</Nav.Link>
+                </Nav.Item>
 
-class UserHomePage extends React.Component{
-    render(){
-        return (
-            <div>
-            <Header isGuest="false" />
-            <h1>UserHomePage works</h1>
-            </div>
-        );
-    }
+                <Nav.Item>
+                  <Nav.Link eventKey="findFriends">Find Friends</Nav.Link>
+                  
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link eventKey="chats">Chats</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link eventKey="questionnaire">Questionnaire</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link eventKey="linkAccounts">Link Accounts</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link eventKey="settings">Settings</Nav.Link>
+                </Nav.Item>
+
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="profile">
+                  <h3>Profile section</h3>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="findFriends">
+                  <h3>Find Friends section</h3>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="chats">
+                  <h3>Chats section</h3>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="questionnaire">
+                  <h3>Questionnaire section</h3>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="linkAccounts">
+                  <h3>Link Accounts section</h3>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="settings">
+                  <h3>Settings section</h3>
+                </Tab.Pane>
+
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </div>
+    );
+  }
 }
 
-export default UserHomePage
+export default UserHomePage;
